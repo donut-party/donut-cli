692143aeff822f7abf1789d3cfe7a93addc8e1bd
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-07 23:21:00 -0400

    fix version refs

--- develop/src/donut/cli/develop/publish.clj
+++ develop/src/donut/cli/develop/publish.clj
@@ -1,18 +1,15 @@
 (ns donut.cli.develop.publish 
   (:require
-   [babashka.fs :as fs]
    [babashka.process :as ps]
    [donut.cli.develop.bump-version :as bump-version]
-   [donut.cli.develop.update-deps :as update-deps]
-   [donut.cli.develop.util :as util]))
+   [donut.cli.develop.update-deps :as update-deps]))
 
 (defn -main
   []
   (update-deps/-main)
   (bump-version/-main)
   (ps/shell "git add .")
-  (let [proot       (util/project-root)
-        version     (slurp (str (fs/path proot "donut-cli/VERSION")))
+  (let [version     (bump-version/version)
         tag-name    (str "v" version)]
     (ps/shell (format "git commit -m 'Bump version %s'" version))
     (ps/shell (str "git tag " tag-name))
