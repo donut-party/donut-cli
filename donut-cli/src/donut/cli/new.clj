46d3f8a128dcf9c9068a9a1b0f9a4a46f15cffc7
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-07 21:59:25 -0400

    fix new

--- donut-cli/src/donut/cli/new.clj
+++ donut-cli/src/donut/cli/new.clj
@@ -1,4 +1,6 @@
-(ns donut.cli.new)
+(ns donut.cli.new 
+  (:require
+   [babashka.cli :as cli]))
 
 (defn- set-classpath
   "Required by deps.new"
@@ -7,15 +9,38 @@
    "java.class.path"
    ((requiring-resolve 'babashka.classpath/get-classpath))))
 
-(def cli-spec
-  {:target-dir {:alias :t}
-   :override   {:alias :o}})
+(def cli-opts
+  {:target-dir {:alias :t
+                :desc  "directory to create the project in"}
+   :overwrite  {:alias :o
+                :desc  "overwrite directory if it exists"}
+   :name       {:alias :n
+                :desc  "project name in group-name/artifact-name format"}})
 
 (def args->opts [:name])
 
+(def cli-spec
+  {:spec       cli-opts
+   :args->opts [:name]})
+
 (defn run-deps-new
-  [{:keys [opts]}]
+  [opts]
   (set-classpath)
   ((requiring-resolve 'org.corfield.new/create)
    (merge opts
           {:template 'party.donut/single-page-app})))
+
+(defn print-help
+  [spec]
+  (println "creates a new Donut single-page app")
+  (println "example: donut my-co/my-project")
+  (println (cli/format-opts (merge spec {:order (vec (keys (:spec spec)))}))))
+
+(defn -main
+  [& args]
+  (if args
+    (let [opts (cli/parse-opts args cli-spec)]
+      (if (or (:help opts) (:h opts))
+        (print-help cli-spec)
+        (run-deps-new opts)))
+    (print-help cli-spec)))
