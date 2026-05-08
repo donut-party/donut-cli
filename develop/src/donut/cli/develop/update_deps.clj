a9ddf25849337a28cbf870a9a9796fabd97c05cd
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-07 22:21:22 -0400

    script to update single page app sha

---
+++ develop/src/donut/cli/develop/update_deps.clj
@@ -0,0 +1,27 @@
+(ns donut.cli.develop.update-deps
+  "update donut-cli bb.edn with most recent deps"
+  (:require
+   [babashka.fs :as fs]
+   [babashka.process :as ps]
+   [clojure.string :as str]
+   [donut.cli.develop.util :as util]
+   [rewrite-clj.zip :as z]))
+
+(defn update-git-sha [zloc dep-sym new-sha]
+  (-> zloc
+      (z/find-value z/next dep-sym)   ; find the dep key (e.g. 'party.donut/single-page-app)
+      (z/find-value z/next :git/sha)  ; find :git/sha within that dep's map
+      z/right                          ; move to the value (the sha string)
+      (z/replace new-sha)))            ; replace with new sha
+
+(defn -main
+  []
+  (let [proot       (util/project-root)
+        latest-sha  (str/trim (:out (ps/sh {:dir (fs/path proot "../single-page-app")} "git rev-parse HEAD")))
+        bb-edn-path (str (fs/path proot "donut-cli/bb.edn"))]
+    (spit bb-edn-path
+          (-> bb-edn-path
+              slurp
+              z/of-string
+              (update-git-sha 'party.donut/single-page-app latest-sha)
+              z/root-string))))
